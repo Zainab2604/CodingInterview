0d107bda055d7ae9a10790797312e6df2871e565
Zainab2604 <150742522+Zainab2604@users.noreply.github.com>
2024-08-20 20:13:22 +0200

    delete debug file

--- cmake-build-debug/lib/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/zainabmohamedbasheer/CLionProjects/CodingInterviewFinal/lib/googletest
-# Build directory: /Users/zainabmohamedbasheer/CLionProjects/CodingInterviewFinal/cmake-build-debug/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/zainabmohamedbasheer/CLionProjects/CodingInterviewFinal/lib/googletest/googletest
-# Build directory: /Users/zainabmohamedbasheer/CLionProjects/CodingInterviewFinal/cmake-build-debug/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
